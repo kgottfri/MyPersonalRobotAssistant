79f8d81768fc7a3feef7c5acdefa78a1ef86780d
Kevin Gottfried <kgott541@gmail.com>
2022-04-06 15:36:55 -0700

    remove keys

--- app.rb
+++ app.rb
@@ -2,100 +2,100 @@ require 'sinatra'
 require 'json'
 require 'http'
 
-get '/' do
-  'Hello World'
-end
+# get '/' do
+#   'Hello World'
+# end
 
-post '/' do
-  data = JSON.parse request.body.read
-  user = data['event']['user']
+# post '/' do
+#   data = JSON.parse request.body.read
+#   user = data['event']['user']
 
-  HTTP.post("https://slack.com/api/chat.postMessage", params: {
-    token: ENV['SLACK_API_TOKEN'],
-    channel: '#recordsponge_new_contributors',
-    text: text(user),
-    blocks: blocks(user)
-  })
-  :ok
-end
+#   HTTP.post("https://slack.com/api/chat.postMessage", params: {
+#     token: ENV['SLACK_API_TOKEN'],
+#     channel: '#recordsponge_new_contributors',
+#     text: text(user),
+#     blocks: blocks(user)
+#   })
+#   :ok
+# end
 
-def text user
-<<-HEREDOC
-Hello <@#{user}>, welcome to the Record Expungement Project!
-Here are some project resources to get started:
-1. Our website: recordsponge.com
-2. Our <https://calendar.google.com/calendar?cid=bGo4aW91MTFwZ2ZlZnE3ZG12MGQ0M2FrcGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ|meetings calendar>
-3. Our <https://github.com/codeforpdx/recordexpungPDX/|github repo>
-4. Our slack channels: #recordsponge #recordsponge_new_contributors #recordsponge_dev #recordsponge_web_presence #recordsponge_legislation-advocacy
-Welcome aboard!
-HEREDOC
-end
+# def text user
+# <<-HEREDOC
+# Hello <@#{user}>, welcome to the Record Expungement Project!
+# Here are some project resources to get started:
+# 1. Our website: recordsponge.com
+# 2. Our <https://calendar.google.com/calendar?cid=bGo4aW91MTFwZ2ZlZnE3ZG12MGQ0M2FrcGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ|meetings calendar>
+# 3. Our <https://github.com/codeforpdx/recordexpungPDX/|github repo>
+# 4. Our slack channels: #recordsponge #recordsponge_new_contributors #recordsponge_dev #recordsponge_web_presence #recordsponge_legislation-advocacy
+# Welcome aboard!
+# HEREDOC
+# end
 
-def blocks user
-  "[
-    {
-      'type': 'section',
-      'text': {
-        'type': 'mrkdwn',
-        'text': 'Hello <@#{user}>, welcome to the Record Expungement Project!'
-      }
-    },
-    {
-      'type': 'section',
-      'text': {
-        'type': 'plain_text',
-        'text': 'Here are some project resources to get started:',
-        'emoji': true
-      }
-    },
-    {
-      'type': 'section',
-      'text': {
-        'type': 'mrkdwn',
-        'text': '1. Our website: recordsponge.com'
-      }
-    },
-    {
-      'type': 'image',
-      'title': {
-        'type': 'plain_text',
-        'text': 'recordsponge.com',
-        'emoji': true
-      },
-      'image_url': 'https://res.cloudinary.com/da0cmt1gu/image/upload/t_media_lib_thumb/v1592229029/Screen_Shot_2020-06-15_at_10.46.47_PM_zm5c1l.png',
-      'alt_text': 'marg'
-    },
-    {
-      'type': 'section',
-      'text': {
-        'type': 'mrkdwn',
-        'text': '2. Our <https://calendar.google.com/calendar?cid=bGo4aW91MTFwZ2ZlZnE3ZG12MGQ0M2FrcGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ|meetings calendar>'
-      }
-    },
-    {
-      'type': 'section',
-      'text': {
-        'type': 'mrkdwn',
-        'text': '3. Our <https://github.com/codeforpdx/recordexpungPDX/|github repo>'
-      }
-    },
-    {
-      'type': 'section',
-      'text': {
-        'type': 'mrkdwn',
-        'text': '4. Our slack channels: #recordsponge #recordsponge_new_contributors #recordsponge_dev #recordsponge_web_presence #recordsponge_legislation-advocacy'
-      }
-    },
-    {
-      'type': 'section',
-      'text': {
-        'type': 'plain_text',
-        'text': 'Welcome aboard!',
-        'emoji': true
-      }
-    },
-    {
-      'type': 'divider'
-    }
-  ]"
-end
+# def blocks user
+#   "[
+#     {
+#       'type': 'section',
+#       'text': {
+#         'type': 'mrkdwn',
+#         'text': 'Hello <@#{user}>, welcome to the Record Expungement Project!'
+#       }
+#     },
+#     {
+#       'type': 'section',
+#       'text': {
+#         'type': 'plain_text',
+#         'text': 'Here are some project resources to get started:',
+#         'emoji': true
+#       }
+#     },
+#     {
+#       'type': 'section',
+#       'text': {
+#         'type': 'mrkdwn',
+#         'text': '1. Our website: recordsponge.com'
+#       }
+#     },
+#     {
+#       'type': 'image',
+#       'title': {
+#         'type': 'plain_text',
+#         'text': 'recordsponge.com',
+#         'emoji': true
+#       },
+#       'image_url': 'https://res.cloudinary.com/da0cmt1gu/image/upload/t_media_lib_thumb/v1592229029/Screen_Shot_2020-06-15_at_10.46.47_PM_zm5c1l.png',
+#       'alt_text': 'marg'
+#     },
+#     {
+#       'type': 'section',
+#       'text': {
+#         'type': 'mrkdwn',
+#         'text': '2. Our <https://calendar.google.com/calendar?cid=bGo4aW91MTFwZ2ZlZnE3ZG12MGQ0M2FrcGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ|meetings calendar>'
+#       }
+#     },
+#     {
+#       'type': 'section',
+#       'text': {
+#         'type': 'mrkdwn',
+#         'text': '3. Our <https://github.com/codeforpdx/recordexpungPDX/|github repo>'
+#       }
+#     },
+#     {
+#       'type': 'section',
+#       'text': {
+#         'type': 'mrkdwn',
+#         'text': '4. Our slack channels: #recordsponge #recordsponge_new_contributors #recordsponge_dev #recordsponge_web_presence #recordsponge_legislation-advocacy'
+#       }
+#     },
+#     {
+#       'type': 'section',
+#       'text': {
+#         'type': 'plain_text',
+#         'text': 'Welcome aboard!',
+#         'emoji': true
+#       }
+#     },
+#     {
+#       'type': 'divider'
+#     }
+#   ]"
+# end
--- lib/tasks/av_meeting_bot.rake
+++ lib/tasks/av_meeting_bot.rake
@@ -1,36 +1,36 @@
 require 'http'
 require 'json'
 
-desc 'Slack a list of AV meetings to general'
-task :meetings do
-  events_url = 'https://www.agileventures.org/events'
+# desc 'Slack a list of AV meetings to general'
+# task :meetings do
+#   events_url = 'https://www.agileventures.org/events'
 
-  header = "<!here> are the events for the next 24 hours. Hosted by AV members.\n\n"
-  events_calendar = "<#{events_url}|Agile Ventures calendar>"
-  footer = "\nCome join us!\nTo stay up to date with our events, check out the #{events_calendar}."
-  one_day = 86400
-  tomorrow = Time.now.utc + one_day
-  rc = HTTP.get("#{events_url}.json")
-  message = header
-  JSON.parse(rc.body).each do |event|
-    if event_start_time(event) < tomorrow
-      message += "#{event['title']}: <!date^#{event_start_time(event).to_i}^{date_short_pretty} at {time}|#{event_start_time(event).utc}>\n"
-    end
-  end
-  message += footer
+#   header = "<!here> are the events for the next 24 hours. Hosted by AV members.\n\n"
+#   events_calendar = "<#{events_url}|Agile Ventures calendar>"
+#   footer = "\nCome join us!\nTo stay up to date with our events, check out the #{events_calendar}."
+#   one_day = 86400
+#   tomorrow = Time.now.utc + one_day
+#   rc = HTTP.get("#{events_url}.json")
+#   message = header
+#   JSON.parse(rc.body).each do |event|
+#     if event_start_time(event) < tomorrow
+#       message += "#{event['title']}: <!date^#{event_start_time(event).to_i}^{date_short_pretty} at {time}|#{event_start_time(event).utc}>\n"
+#     end
+#   end
+#   message += footer
 
-  unless message.length > (header + footer).length
-    message = "No events were found for today: Happy coding!"
-  end
+#   unless message.length > (header + footer).length
+#     message = "No events were found for today: Happy coding!"
+#   end
 
-  HTTP.post("https://slack.com/api/chat.postMessage", params: {
-    token: ENV['AV_POPPA_NICK_TOKEN'],
-    channel: '#general',
-    text: message
-  })
+#   HTTP.post("https://slack.com/api/chat.postMessage", params: {
+#     token: ENV['AV_POPPA_NICK_TOKEN'],
+#     channel: '#general',
+#     text: message
+#   })
 
-end
+# end
 
-def event_start_time(event)
-  Time.strptime(event['start'] + '+0000', '%Y-%m-%dT%H:%M:%S%z')
-end
+# def event_start_time(event)
+#   Time.strptime(event['start'] + '+0000', '%Y-%m-%dT%H:%M:%S%z')
+# end
